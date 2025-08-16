2a572e48ce21fe49d28c0c3574ca326a4f1d91f3
Swartea <131281605+Swartea@users.noreply.github.com>
2025-08-16 03:59:28 -0400

    Start commit numbering at 1

--- create_backdated_commits.sh
+++ create_backdated_commits.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-for i in $(seq 0 11); do
+for i in $(seq 1 12); do
   ts=$(date -u -d "2025-06-10 06:00:00 UTC + $((i*5)) minutes" "+%Y-%m-%dT%H:%M:%S")
   echo "Backdated commit $i at $ts" >> README.md
   git add README.md
